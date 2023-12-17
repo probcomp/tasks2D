beabb60d1a685459521beddaa3e74101a789a14b
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2023-12-17 16:33:17 -0500

    Turn map generation into a distribution so we can constrain its value

--- notebooks_messy/SLAM/distributions.jl
+++ notebooks_messy/SLAM/distributions.jl
@@ -1,30 +1,26 @@
 """
-Tasks2D.Distributions
-
-Some Gen distributions used in the grid tasks.
+Some Gen distributions used in the SLAM tasks.
 """
+
 using Gen
-import FunctionalCollections: PersistentVector
 import GridWorlds
 
-# Snippets to sample map (when a map is not given)
-# Use IID Bernoulli to sample each map cell
-# @dist flip_wall(wall_prob::Real) = bernoulli(wall_prob)) ? GridWorlds.wall : GridWorlds.empty
-# @dist flip_wall(wall_prob::Real) = [GridWorlds.wall, GridWorlds.empty][categorical([wall_prob, 1-wall_prob])]
-# ^ for whatever reason this errors out because bernoulli(wall_prob) cannot be interpreted as Bool
+struct BernoulliMap <: Gen.Distribution{GridWorlds.GridWorld} end
 
-@gen (static) function generate_gridcell(wall_prob::Real)
-    is_wall ~ bernoulli(wall_prob)
-    return is_wall ? GridWorlds.wall : GridWorlds.empty
+function Gen.random(::BernoulliMap, width::Int, height::Int, wall_prob::Real)
+    cells = Matrix{GridWorlds.GridCell}(undef, width, height)
+    for i in 1:width
+        for j in 1:height
+            cells[i, j] = bernoulli(wall_prob) ? GridWorlds.wall : GridWorlds.empty
+        end
+    end
+    return GridWorlds.FGridWorld(cells, nothing, (width, height))
 end
 
-@gen (static) function generate_iid_bernoulli_map(width::Int, height::Int, wall_prob::Real)
-    raw_map ~ Map(Map(generate_gridcell))(fill(fill(wall_prob, height), width))
-    return GridWorlds.FGridWorld(raw_map, nothing, (width, height))
+function Gen.logpdf(::BernoulliMap, w::GridWorlds.GridWorld, width::Int, height::Int, wall_prob::Real)
+    num_empty = length(GridWorlds.empty_cells(w))
+    num_walls = width * height - num_empty
+    return log(wall_prob) * num_walls + log(1 - wall_prob) * num_empty
 end
-
-# Handle PersistentVector{Any} returns by the Map combinator
-function GridWorlds.FGridWorld(grid::PersistentVector{PersistentVector{Any}}, pos::Union{Nothing,Tuple{Int,Int}}, size::Tuple{Int,Int})
-    grid = PersistentVector(PersistentVector{GridWorlds.GridCell}.(grid))
-    GridWorlds.FGridWorld(grid, pos, size)
-end
+bernoulli_map = BernoulliMap()
+(::BernoulliMap)(args...) = random(bernoulli_map, args...)
