d5c705e55658c79afd872b880e3d97bbf6d72f3e
Xiaoyan Wang <xxiaoyanwang@gmail.com>
2023-12-18 00:02:32 -0500

    PF utilities for specifying additional constraints

---
+++ notebooks_messy/SLAM/pf.jl
@@ -0,0 +1,32 @@
+import GenPOMDPs
+import GenParticleFilters
+import Gen
+
+function pf(pomdp, params, pf_initialize_params, pf_update_params, init_constraints::ChoiceMap, pre_update=(_ -> ()),
+    post_update=(_ -> ()))
+    return (
+        pf_initializer(pomdp, params, pf_initialize_params, init_constraints),
+        GenPOMDPs.pf_updater(pf_update_params...; pre_update, post_update)
+    )
+end
+
+# override default pf to allow for additional constraints
+function pf_initializer(pomdp::GenPOMDPs.GenPOMDP, pomdp_params, pf_params, init_constraints::ChoiceMap)
+    controlled_trajectory_model = GenPOMDPs.ControlledTrajectoryModel(pomdp)
+
+    function initialize(obs0)
+        constraints = choicemap()
+        Gen.set_submap!(constraints, GenPOMDPs.state_addr(0), init_constraints)
+        Gen.set_submap!(constraints, GenPOMDPs.obs_addr(0), obs0)
+
+        pf_state = GenParticleFilters.pf_initialize(
+            controlled_trajectory_model,
+            (0, [], pomdp_params),
+            constraints,
+            pf_params...
+        )
+        return pf_state
+    end
+
+    return initialize
+end
